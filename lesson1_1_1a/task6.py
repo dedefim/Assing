6ce002d642f7f026772390ab39dd6c590c4c073a
efim <dedefim56@gmail.com>
2023-05-27 15:56:05 +0500

    lesson1_1_1a

---
+++ lesson1_1_1a/task6.py
@@ -0,0 +1,18 @@
+"""
+Создать текстовый файл test_file.txt, заполнить его тремя строками:
+«сетевое программирование», «сокет», «декоратор».
+Проверить кодировку файла по умолчанию.
+Принудительно открыть файл в формате Unicode и вывести его содержимое.
+"""
+
+from chardet import detect
+
+with open('text1.txt', encoding='utf-8') as file:
+    for line in file.read():
+        print(line)
+#file.close()
+
+
+file = open('text1.txt', 'rb')
+for line in file:
+    print(line.decode(encoding='utf-8'))
